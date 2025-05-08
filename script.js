8b35903ecfd7624d07690fc36c9c06d535d685be
ranandasatria <rananda24@gmail.com>
2025-05-08 20:47:16 +0700

    refactor: change manual for loop to foreach

--- script.js
+++ script.js
@@ -5,24 +5,25 @@ const gelasC = document.getElementById('gelasC')
 const arrayGelas = [gelasA, gelasB, gelasC]
 
 function clickCup() {
-    for (let i = 0; i < arrayGelas.length; i++){
-        let gelas = arrayGelas[i]
-        if (gelas.className == 'gelas teh' || gelas.className == 'gelas kopi') {
+    arrayGelas.forEach(gelas => {
+        if (gelas.className === 'gelas teh' || gelas.className === 'gelas kopi') {
             gelas.addEventListener('click', moveContent)
-        }else{
+        } else {
             gelas.removeEventListener('click', moveContent)
         }
-    }
+    })
 }
 
 function moveContent(event){
     const gelasBerisi = event.target
     let gelasKosong;
-    for (let i = 0; i < arrayGelas.length; i++){
-        if (arrayGelas[i].className=='gelas kosong'){
-            gelasKosong = arrayGelas[i]
+
+    arrayGelas.forEach(gelas => {
+        if (gelas.className === 'gelas kosong') {
+            gelasKosong = gelas
         }
-    }
+    })
+   
     gelasKosong.className = gelasBerisi.className
     gelasKosong.textContent = gelasBerisi.textContent
     gelasBerisi.className = 'gelas kosong'
